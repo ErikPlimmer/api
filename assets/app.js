f1a7542cab51ecd3036a03a40f9c49227f61a9e5
Erik Plimmer <Elp013@Gmail.com>
2018-01-16 22:37:43 -0500

    update1/16

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
-var animalArray =["monkey", "lion", "beaver", "snake"];
+var animalArray =["monkey", "lion", "hamster", "snake"];
 
+//call renderButtons function
 
 function renderButtons() {
 	
 	$("#buttons").empty();
-
+ console.log("array is" , animalArray);
 	for (var i=0; i < animalArray.length; i++){
 
 	    var a = $("<button>");
@@ -16,28 +17,26 @@ function renderButtons() {
 	    a.text(animalArray[i]);
 	    // Adding the button to the HTML
 	    $("#buttons").append(a);
-
-   		
   	}
 }// renderButtons function end
-
-//call renderButtons function
 renderButtons();
-  // =================================================================
 
 
+$("#add-button").on("click", function(event) {
+	 event.preventDefault();
+	  var input = $("#button-input").val().trim();
+	  animalArray.push(input);
+	  $("#button-input").val("");
+renderButtons();
 
+ 
  // This .on("click") function will trigger the AJAX Call
- $(".animal").on("click", function giphy(event) {
- 			
-
+ $(".animal").on("click", function (event) {
 	    event.preventDefault();
-
-	     var input = $(this).attr("data-name");
+	    var input = $(this).attr("data-name");
 	    // Here we construct our URL
 	    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=JVLv3VEF12LSVuKjtAyPRJqws2Xvg2pO&limit=10";
 	   console.log(queryURL);
-
 
 	    $.ajax({
 	    url: queryURL,
@@ -50,54 +49,108 @@ renderButtons();
 
 	for (var i = 0; i < results.length; i++) {
 	   	 
+	   	 var animalDiv = $("<div>");
+
 			var rating = results[i].rating;
 
           	var p = $("<p>").text("Rating: " + rating);
 
           	var animalImage = $("<img>");
-          	animalImage.attr("src", results[i].images.fixed_height.url);
+          	animalImage.addClass("thing");
+          	animalImage.attr("src", results[i].images.fixed_height_still.url);
+            animalImage.attr({'data-animate' : results[i].images.fixed_height.url});
+            animalImage.attr({'data-state' : "still"});
+            animalImage.attr({'data-still' : results[i].images.fixed_height_still.url});
+     
 
-         	 $("#results").prepend(p);
-        	  $("#results").prepend(animalImage);
 
+         	 animalDiv.prepend(p);
+        	 animalDiv.prepend(animalImage);
+
+        	  $("#results").prepend(animalDiv);
+
+        	  // console.log("data state is" , response.data-state);
+        	  console.log("results is" , results[i]);
+        	  console.log("input is" ,input);
+        	  // starting and stoping animation funtion here
+          	
+    }
+    $(".thing").on("click", function() {
+          		console.log("in the click image");
+          	var state = $(this).attr("data-state");
+				  if (state === "still") {
+				    $(this).attr("src", $(this).attr("data-animate"));
+				    $(this).attr("data-state", "animate");
+				  } else {
+				    $(this).attr("src", $(this).attr("data-still"));
+				    $(this).attr("data-state", "still");
+				  }
+				});// thing function end 
+    		
+  });//done
+});
+
+});
+
+ $(".animal").on("click", function (event) {
+	    event.preventDefault();
+
+	    var input = $(this).attr("data-name");
+	    // Here we construct our URL
+	    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=JVLv3VEF12LSVuKjtAyPRJqws2Xvg2pO&limit=10";
+	   
+	   console.log(queryURL);
+
+	    $.ajax({
+	    url: queryURL,
+	    method: "GET"
+	  }).done(function(response) {
+
+  			var results = response.data;
+
+	        $("#results").append(response);
+
+	for (var i = 0; i < results.length; i++) {
+	   	 
+	   	 var animalDiv = $("<div>");
+
+			var rating = results[i].rating;
+
+          	var p = $("<p>").text("Rating: " + rating);
+
+          	var animalImage = $("<img>");
+          	animalImage.attr("src", results[i].images.fixed_height_still.url);
+          	animalImage.addClass("thing");
+            animalImage.attr({'data-animate' : results[i].images.fixed_height.url});
+            animalImage.attr({'data-state' : "still"});
+            animalImage.attr({'data-still' : results[i].images.fixed_height_still.url});
+
+
+         	 animalDiv.prepend(p);
+        	 animalDiv.prepend(animalImage);
+
+        	  $("#results").prepend(animalDiv);
+
+        	  // console.log("data state is" , response.data-state);
         	  console.log("results is" , results[i]);
         	  console.log("input is" ,input);
 
 
     }
+    	$(".thing").on("click", function() {
+          		console.log("in the click image");
+          	var state = $(this).attr("data-state");
+				  if (state === "still") {
+				    $(this).attr("src", $(this).attr("data-animate"));
+				    $(this).attr("data-state", "animate");
+				  } else {
+				    $(this).attr("src", $(this).attr("data-still"));
+				    $(this).attr("data-state", "still");
+				  }
+				});// thing function end 
     		
-    	
-	        
-
-	
-
-  });//click event function end 
-
-  $("#add-button").on("click", function(event) {
- 		    event.preventDefault();
- 		 // Here we grab the text from the input box
-	    var input = $("#button-input").val().trim();
-
-	    	animalArray.push(input);
-
-	    	for (var i=0; i < animalArray.length; i++){
-
-	    	   var a = $("<button>");
-				    // Adding a class
-				    a.addClass("animal");
-				    // Adding a data-attribute with a value of the array at index i
-				    a.attr("data-name", animalArray[i]);
-				    // Providing the button's text with a value of the array at index i
-				    a.text(animalArray[i]);
-				    // Adding the button to the HTML
-				    $("#buttons").append(a);
-
-				    renderButtons();
-				    
-				    console.log("add-button", input);
-			}
-
- }); // add button function end 
-
-
+  });//done
+	  // starting and stoping animation funtion here
+          
 });
+
